d57d20cd83ce4f9351285494653660b35e16484f
hsuBnOediH <li.8906@buckeyemail.osu.edu>
2024-11-04 11:43:48 -0800

    update the subb file

---
+++ runall_advise_comparison.py
@@ -0,0 +1,31 @@
+import sys, os, re, subprocess
+
+subject_list_path = '/mnt/dell_storage/labs/rsmith/lab-members/fli/advise_task/subject_id/advise_subject_IDs_prolific_wo_uncomplete.csv'
+
+
+results = '/mnt/dell_storage/labs/rsmith/lab-members/fli/advise_task/results/model_comparison/'
+
+if not os.path.exists(results):
+    os.makedirs(results)
+    print(f"Created results directory {results}")
+
+if not os.path.exists(f"{results}/logs"):
+    os.makedirs(f"{results}/logs")
+    print(f"Created results-logs directory {results}/logs")
+
+subjects = []
+with open(subject_list_path) as infile:
+    for line in infile:
+        if 'ID' not in line:
+            subjects.append(line.strip())
+
+ssub_path = '/mnt/dell_storage/labs/rsmith/lab-members/fli/advise_task/Active-Inference-Model-for-Advise-Task/run_advise_model_comparison.ssub'
+
+for idx_candidate in [3,4,5,6,7,8,9,10]:
+    for subject in subjects:
+        stdout_name = f"{results}/logs/{subject}-%J.stdout"
+        stderr_name = f"{results}/logs/{subject}-%J.stderr"
+        jobname = f'advise-comparison-{subject}-{idx_candidate}'
+        os.system(f"sbatch -J {jobname} -o {stdout_name} -e {stderr_name} {ssub_path} {subject} {results} {idx_candidate}")
+
+        print(f"SUBMITTED JOB [{jobname}]")
